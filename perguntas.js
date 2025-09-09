f91ba37d202bd30c4a7ad56da4fb26b1e04e6ecb
casanova8448 <guilherme.casanova@escola.pr.gov.br>
2025-09-09 08:47:04 -0300

    09/09

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'Russia'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Quem foi o autor da teoria da gravidade?',
+    'Isaac Newton'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Em que continente fica a Cordilheira dos Andes?',
+    ' América do Sul'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é a capital da Austrália?',
+    'Camberra'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual elemento químico tem o símbolo "O"?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Quem pintou a obra "A Última Ceia"?',
+    '  Leonardo da Vinci'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'pergunta',
+    'resposta'
+)

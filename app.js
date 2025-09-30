d008b78a07da885a22b2df6e014ca9e829c8027f
casanova8448 <guilherme.casanova@escola.pr.gov.br>
2025-09-30 10:22:48 -0300

    30/09

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ function criaCartao(categoria, pergunta, resposta) {
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-    <p>${pergunta} - escrever a pergunta</p>
+    <p>${pergunta}</p>
     </div>
      <div class="cartao__conteudo__resposta">
-    <p>${resposta}- escrever a resposta</p>
+    <p>${resposta}</p>
     </div>
     </div>`
 
